6037892d7d5a1028c82b4885eec620798b9bdb82
alessandro.putzu <alessandro.putzu@gmail.com>
2015-04-13 17:18:53 +0200

    Re-initial import.

---
+++ tb-embargo-connector/src/main/java/it/sia/tonbeller/embargo/connector/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.sia.tonbeller.embargo.connector;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TbEmbargoConnectorApplication.class);
+	}
+
+}
---
+++ tb-embargo-connector/src/main/java/it/sia/tonbeller/embargo/connector/TbEmbargoConnectorApplication.java
@@ -0,0 +1,12 @@
+package it.sia.tonbeller.embargo.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbEmbargoConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbEmbargoConnectorApplication.class, args);
+    }
+}
